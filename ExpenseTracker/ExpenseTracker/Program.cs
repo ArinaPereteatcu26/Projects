e8e41db143cdd4249a1c575a5caf7a9da61f23c8
Arina <arinapereteatcu0@gmail.com>
2025-01-16 20:55:28 +0200

    Add Categories

--- ExpenseTracker/ExpenseTracker/Program.cs
+++ ExpenseTracker/ExpenseTracker/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
 
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCf0x0R3xbf1x1ZFRGalxRTnRbUiweQnxTdEBjWH5bcXRVRmJYWUV+WQ==");
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//DI
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DevConnection")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
