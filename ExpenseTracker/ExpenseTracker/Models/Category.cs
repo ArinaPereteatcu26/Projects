79c586a8294d3c98c9c747866384af8aaf2b50c9
Arina <arinapereteatcu0@gmail.com>
2025-02-16 17:28:19 +0200

    Add Transaction controller

--- ExpenseTracker/ExpenseTracker/Models/Category.cs
+++ ExpenseTracker/ExpenseTracker/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata;
 
 namespace ExpenseTracker.Models
 {
@@ -8,6 +9,7 @@ namespace ExpenseTracker.Models
         [Key] public int CategoryId { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
 
         [Column(TypeName = "nvarchar(5)")] 
